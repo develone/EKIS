0e29fd1fce9887a58d0795d9e1670dfbf95fba8b
Edward Vidal Jr <develone@sbcglobal.net>
2017-07-23 01:43:04 +0000

    collecting GPS w/pi-zero W and computing stats

---
+++ octave/stats.m
@@ -0,0 +1,18 @@
+filename = 'gps.csv';
+x = dlmread(filename,',');
+
+figure
+hist(x(1:41913,10))
+title 'ELEVATION m'
+std_elev = std(x(1:41913,10))
+
+figure
+hist(x(1:41913,3))
+title 'LATITUDE N'
+std_lat = std(x(1:41913,3))
+
+figure
+hist(x(1:41913,5))
+title 'LONGITUDE W'
+std(x(1:41913,5))
+std_long = std(x(1:41913,5))
