7c3cf061aa6706648f54ae8d864b6031a1f8a6a4
Edward Vidal Jr <develone@sbcglobal.net>
2017-07-24 15:13:00 +0000

    adding more stats and plots of elevation, lat, and long

--- octave/stats.m
+++ octave/stats.m
@@ -1,22 +1,55 @@
 filename = 'gps.csv';
-x = dlmread(filename,',');
-
+m = dlmread(filename,',');
+x = 1:81596;
 figure
-hist(x(1:41913,10))
+elev = m(x,10);
+hist(elev)
 title 'ELEVATION m'
-std_elev = std(x(1:41913,10))
+
+
+std_elev = std(elev)
+mean_elev = mean(elev)
+min_elev = min(elev)
+max_elev = max(elev)
 
 figure
-hist(x(1:41913,3))
+lat = m(x,3);
+hist(lat)
 title 'LATITUDE N'
-std_lat = std(x(1:41913,3))
+std_lat = std(lat)
+mean_lat = mean(lat)
+min_lat = min(lat)
+max_lat = max(lat)
 
 figure
-hist(x(1:41913,5))
+long = m(x,5);
+hist(long)
 title 'LONGITUDE W'
-std(x(1:41913,5))
-std_long = std(x(1:41913,5))
+std(m(x,5))
+std_long = std(long)
+mean_long = mean(long)
+min_long = min(long)
+max_long = max(long)
 
 figure
-hist(x(1:81596,8))
+hist(m(x,8))
 title 'NUMBER OF SAT'
+
+figure
+plot(x,elev)
+title 'ELEVATION m'
+xlabel 'sample'
+ylabel 'elev' 
+
+figure
+plot(x,lat)
+title 'LATITUDE'
+xlabel 'sample'
+ylabel 'latv' 
+
+figure
+plot(x,long)
+title 'LONGITUDE'
+xlabel 'sample'
+ylabel 'long' 
+
